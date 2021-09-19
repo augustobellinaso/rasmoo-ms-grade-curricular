b68fb3ef8d47b6a293db5d7d853a0563ccb7ae58
Augusto Bellinaso <augustobellinaso@gmail.com>
2021-09-19 17:55:01 -0300

    implementando camada cursoservice

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/service/CursoService.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/service/CursoService.java
@@ -1,10 +1,16 @@
 package com.rasmoo.cliente.escola.gradecurricular.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import com.rasmoo.cliente.escola.gradecurricular.constante.Mensagens;
 import com.rasmoo.cliente.escola.gradecurricular.entity.CursoEntity;
+import com.rasmoo.cliente.escola.gradecurricular.entity.MateriaEntity;
+import com.rasmoo.cliente.escola.gradecurricular.exception.CursoException;
 import com.rasmoo.cliente.escola.gradecurricular.model.CursoModel;
 import com.rasmoo.cliente.escola.gradecurricular.repository.ICursoRepository;
 import com.rasmoo.cliente.escola.gradecurricular.repository.IMateriaRepository;
@@ -24,26 +30,92 @@ public class CursoService implements ICursoService{
 
     @Override
     public Boolean cadastrar(CursoModel cursoModel) {
-        return null;
+        try {
+            if (cursoModel.getId() != null) {
+                throw new CursoException(Mensagens.ERRO_ID_INFORMADO.getDescricao(), HttpStatus.BAD_REQUEST);
+            }
+
+            if (this.cursoRepository.findCursoByCodigo(cursoModel.getCodCurso()) != null) {
+                throw new CursoException(Mensagens.ERRO_CURSO_CADASTRADO_ANTERIORMENTE.getDescricao(), HttpStatus.BAD_REQUEST);
+            }
+            return this.cadastrarOuAtualizar(cursoModel);
+        } catch (CursoException c) {
+            throw c;
+        } catch (Exception e) {
+            throw new CursoException(Mensagens.ERRO_GENERICO.getDescricao(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
     public Boolean atualizar(CursoModel cursoModel) {
-        return null;
+        try {
+            this.consultarPorCodigo(cursoModel.getCodCurso());
+            return this.cadastrarOuAtualizar(cursoModel);
+        } catch (CursoException c) {
+            throw c;
+        } catch (Exception e) {
+            throw e;
+        }
     }
 
     @Override
     public Boolean excluir(Long cursoId) {
-        return null;
+        try {
+            if(this.cursoRepository.findById(cursoId).isPresent()) {
+                this.cursoRepository.deleteById(cursoId);
+                return Boolean.TRUE;
+            }
+            throw new CursoException(Mensagens.ERRO_CURSO_NAO_ENCONTRADO.getDescricao(), HttpStatus.NOT_FOUND);
+        }catch (CursoException c) {
+            throw c;
+        }catch (Exception e) {
+            throw new CursoException(Mensagens.ERRO_GENERICO.getDescricao(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
+    @CachePut(key = "#codCurso")
     public CursoEntity consultarPorCodigo(String codCurso) {
-        return null;
+        try {
+            CursoEntity curso = this.cursoRepository.findCursoByCodigo(codCurso);
+
+            if (curso == null) {
+                throw new CursoException(Mensagens.ERRO_CURSO_NAO_ENCONTRADO.getDescricao(), HttpStatus.NOT_FOUND);
+            }
+
+            return curso;
+        } catch (CursoException c) {
+            throw c;
+        } catch (Exception e) {
+            throw new CursoException(Mensagens.ERRO_GENERICO.getDescricao(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
+    @CachePut(unless = "#result.size()<3")
     public List<CursoEntity> listarCursos() {
-        return null;
+        return this.cursoRepository.findAll();
+    }
+
+    private Boolean cadastrarOuAtualizar(CursoModel cursoModel) {
+        List<MateriaEntity> materiaEntityList = new ArrayList<>();
+
+        if (!cursoModel.getMaterias().isEmpty()) {
+            cursoModel.getMaterias().forEach(materia -> {
+                if (this.materiaRepository.findById(materia).isPresent()) {
+                    materiaEntityList.add(this.materiaRepository.findById(materia).get());
+                }
+            });
+        }
+
+        CursoEntity cursoEntity = new CursoEntity();
+        if (cursoEntity.getId() != null) {
+            cursoEntity.setId(cursoModel.getId());
+        }
+        cursoEntity.setCodigo(cursoModel.getCodCurso());
+        cursoEntity.setNome(cursoModel.getNome());
+        cursoEntity.setMaterias(materiaEntityList);
+
+        return Boolean.TRUE;
     }
 }
