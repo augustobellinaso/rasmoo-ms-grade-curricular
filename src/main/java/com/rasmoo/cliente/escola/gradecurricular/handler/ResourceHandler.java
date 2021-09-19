bbc201cb6c53d9dc1c943a19a1477e7f5278233f
Augusto Bellinaso <augustobellinaso@gmail.com>
2021-09-19 14:23:33 -0300

    tratando o @valid

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/handler/ResourceHandler.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/handler/ResourceHandler.java
@@ -1,6 +1,11 @@
 package com.rasmoo.cliente.escola.gradecurricular.handler;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import com.rasmoo.cliente.escola.gradecurricular.exception.MateriaException;
@@ -9,6 +14,17 @@ import com.rasmoo.cliente.escola.gradecurricular.model.ErrorResponse.ErrorRespon
 
 @ControllerAdvice
 public class ResourceHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String campo = ((FieldError) error).getField();
+            String mensagem = error.getDefaultMessage();
+            errors.put(campo, mensagem);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
 
     @ExceptionHandler(MateriaException.class)
     public ResponseEntity<ErrorResponse> handlerMateriaException(MateriaException ex) {
